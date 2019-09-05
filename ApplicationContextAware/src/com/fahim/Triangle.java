97b0635f210f8d0ad3dd971d27f6bd7eb7ed4d6a
fahu26 <fahimshaikh2606@gmail.com>
2019-09-05 23:50:25 +0530

    Example implemented

---
+++ ApplicationContextAware/src/com/fahim/Triangle.java
@@ -0,0 +1,46 @@
+package com.fahim;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware,BeanNameAware {
+private Point pointA;
+private Point pointB;
+private Point pointC;
+private ApplicationContext context;
+public Point getPointA() {
+	return pointA;
+}
+public void setPointA(Point pointA) {
+	this.pointA = pointA;
+}
+public Point getPointB() {
+	return pointB;
+}
+public void setPointB(Point pointB) {
+	this.pointB = pointB;
+}
+public Point getPointC() {
+	return pointC;
+}
+public void setPointC(Point pointC) {
+	this.pointC = pointC;
+}
+
+public void draw() {
+	System.out.println("Point A: "+pointA.getX()+","+pointA.getY());
+	System.out.println("Point B: "+pointB.getX()+","+pointB.getY());
+	System.out.println("Point C: "+pointC.getX()+","+pointC.getY());
+	
+}
+@Override
+public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+this.context = context;	
+}
+@Override
+public void setBeanName(String beanName) {
+System.out.println("BeanName is : "+beanName);	
+}
+}
